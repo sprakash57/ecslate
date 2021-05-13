b7aae745cc18573459cf8aa04cfb49dfad952520
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-05-13 22:53:06 +0530

    Editor: working on the functionality

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,16 +1,27 @@
 import { FunctionalComponent, h } from 'preact';
+import { useRef } from 'preact/hooks';
 import style from './style.css';
 
 const Home: FunctionalComponent = () => {
+    const editor = useRef<HTMLTextAreaElement>();
+    const output = useRef<HTMLTextAreaElement>()
+    const clearEditor = (): void => {
+        editor.current.value = "";
+    }
+    const runEditor = () => {
+        output.current.value = eval(editor.current.value);
+    }
     return (
         <main class={style.container}>
             <section class={style.section}>
                 <h2>Editor</h2>
-                <textarea name="editor" id="editor" cols={60} rows={38} />
+                <button onClick={runEditor}>Run</button>
+                <button onClick={clearEditor}>Clear</button>
+                <textarea ref={editor} name="editor" id="editor" cols={60} rows={38} />
             </section>
             <section class={style.section}>
                 <h2>Output</h2>
-                <textarea name="output" cols={60} rows={16} readonly />
+                <textarea ref={output} name="output" cols={60} rows={16} readonly />
                 <h2>Console</h2>
                 <textarea name="console" cols={60} rows={17} readonly />
             </section>
