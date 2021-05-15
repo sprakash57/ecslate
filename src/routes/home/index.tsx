e14354b7ec89ca178883f7f2cb60df60d4b1d7b4
Sunny Prakash <sunny.prakashgm@gmail.com>
2021-05-16 00:26:21 +0530

    feat: able to capture console.log and return both into dom

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -4,12 +4,35 @@ import style from './style.css';
 
 const Home: FunctionalComponent = () => {
     const editor = useRef<HTMLTextAreaElement>();
-    const output = useRef<HTMLTextAreaElement>()
+    const output = useRef<HTMLDivElement>()
     const clearEditor = (): void => {
         editor.current.value = "";
     }
-    const runEditor = () => {
-        output.current.value = eval(editor.current.value);
+    const runEditor = (): void => {
+        (function (): void {
+            function createLogNode(message: string): HTMLDivElement {
+                const node = document.createElement("div");
+                const textNode = document.createTextNode(message);
+                node.appendChild(textNode);
+                return node;
+            }
+            const baseLogFunction = console.log;
+            console.log = function () {
+                baseLogFunction.apply(console, arguments);
+                const args = Array.prototype.slice.call(arguments);
+                for (let i = 0; i < args.length; i++) {
+                    const node = createLogNode(args[i]);
+                    output.current.appendChild(node);
+                }
+
+            }
+
+            window.onerror = function (message, url, linenumber) {
+                console.log("JavaScript error: " + message + " on line " +
+                    linenumber + " for " + url);
+            };
+        })();
+        console.log(eval(editor.current.value));
     }
     return (
         <main class={style.container}>
@@ -21,7 +44,7 @@ const Home: FunctionalComponent = () => {
             </section>
             <section class={style.section}>
                 <h2>Output</h2>
-                <textarea ref={output} name="output" cols={60} rows={16} readonly />
+                <div ref={output}>Output</div>
                 <h2>Console</h2>
                 <textarea name="console" cols={60} rows={17} readonly />
             </section>
